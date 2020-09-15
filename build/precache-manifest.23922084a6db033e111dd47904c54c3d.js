93fe5a0874e4e6c23fa00bd7012083ec1b9f382a
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-09-15 13:59:42 -0400

    add moviecard materialize

---
+++ build/precache-manifest.23922084a6db033e111dd47904c54c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f46ca9f2ca06a1bb4e1421f0eb43d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f713dbb323b69c85243a",
+    "url": "/static/css/main.4d112193.chunk.css"
+  },
+  {
+    "revision": "9a97146bc4c3b010e483",
+    "url": "/static/js/2.064af430.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.064af430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f713dbb323b69c85243a",
+    "url": "/static/js/main.eff9412d.chunk.js"
+  },
+  {
+    "revision": "6912a1edb0f9ef78ed97",
+    "url": "/static/js/runtime-main.11dfd0f1.js"
+  }
+]);
