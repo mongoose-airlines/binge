72b7dbd82c2ba84141a2ca6b7578ed29751f9e8c
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-16 09:32:05 -0400

    tv booty

--- controllers/tvshows.js
+++ controllers/tvshows.js
@@ -1,5 +1,13 @@
 const Tvshow = require('../models/tvshow');
 
 module.exports = {
+    create,
+}
 
+function create(req, res) {
+    req.body.addedBy = req.user._id
+    req.body.cast = req.body.cast.split(',');
+    Tvshow.create(req.body)
+        .then(tvshow => { res.json(tvshow) })
+        .catch(err => { res.json(err) })
 }
