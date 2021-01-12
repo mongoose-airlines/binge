aacd50e32feeba81626a48b7f4370c821d8afe83
jpaige343 <jenniferoneill129@gmail.com>
2021-01-12 10:15:40 -0600

    add movie function

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -6,6 +6,8 @@ import Login from '../Login/Login';
 import Signup from '../Signup/Signup';
 import authService from '../../services/authService';
 import Landing from '../Landing/Landing'
+import AddMovie from '../AddMovie/AddMovie'
+import * as movieAPI from '../../services/movies-api'
 
 class App extends Component {
   state = {
@@ -20,6 +22,14 @@ class App extends Component {
 
   handleSignupOrLogin = () => {
     this.setState({user: authService.getUser()});
+  }
+
+  handleAddMovie = async newMovieData => {
+    const newMovie = await movieAPI.create(newMovieData);
+    newMovie.addedBy = {name: this.state.user.name, _id: this.state.user._id}
+    this.setState(state => ({
+      movies: [...state.movies, newMovie]
+    }), () => this.props.history.push('/movies'));
   }
 
   render () {
@@ -44,6 +54,19 @@ class App extends Component {
             handleSignupOrLogin={this.handleSignupOrLogin}
           />
         }/>
+        <Route
+          exact path='/movies/add'
+          render={() =>
+          authService.getUser() ?
+          <AddMovie
+            handleAddMovie = {this.handleAddMovie}
+            user= {this.state.user}
+          />
+          :
+          <Redirect to='/login' />
+          }
+
+        />
       </>
     );
   }
