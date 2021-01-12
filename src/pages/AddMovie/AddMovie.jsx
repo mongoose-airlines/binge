dc2d677d9be16c4beb842c4768e8543319a14e80
erindoman <erin.doman@gmail.com>
2021-01-12 09:59:38 -0600

    create addmove component

---
+++ src/pages/AddMovie/AddMovie.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './AddMovie.css'
+
+class AddMovie extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h3>Add Movie Page</h3>
+         );
+    }
+}
+ 
+export default AddMovie;
