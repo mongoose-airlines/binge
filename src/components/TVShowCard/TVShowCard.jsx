1335ab8f781e446722ba67298383fbe8104f6c07
ManliestBen <benjamintmanley@gmail.com>
2021-01-14 13:21:53 -0600

    add tvshow card

--- src/components/TVShowCard/TVShowCard.jsx
+++ src/components/TVShowCard/TVShowCard.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function TVShowCard() {
+function TVShowCard({ user, tvshow, handleDeleteTVShow }) {
   return(
-    <h3>TV Show Card</h3>
+    <>
+            <div className=" card">
+                <div className="card-image waves-effect waves-block waves-light">
+                    <img alt="tvshow" className="activator" src={tvshow.image ? tvshow.image : "https://www.cebodtelecom.com/wp-content/uploads/2014/09/related_post_no_available_image.png"} onClick={()=> {}}/>
+                </div>
+                <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">{tvshow.name}<i className="material-icons right">more_vert</i></span>
+                    <p>{tvshow.description}</p>
+                </div>
+                <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">{tvshow.name}<i className="material-icons right">close</i></span>
+                    <h6>Added By:  {tvshow.addedBy.name}</h6>
+                    <h6>IMDB Rating: {tvshow.imdbRating}</h6>
+                    <div>Release Year:  {tvshow.releaseDate}</div>
+                    <div>Cast: {tvshow.cast.join(', ')}</div>
+                    <div>Seasons:  {tvshow.seasons}</div>
+                    <div>Episodes:  {tvshow.episodes}</div>
+                    <p>{tvshow.description}</p>
+                    {user && (user._id === tvshow.addedBy._id) &&
+                        <>
+                            <button type="submit" className="btn red" onClick={() => handleDeleteTVShow(tvshow._id)}>
+                            <i className="material-icons left">delete</i>    
+                                Delete TV Show
+                            </button>
+                            <Link 
+                                className="btn yellow black-text"
+                                to={{
+                                    pathname: '/editTV',
+                                    state: {tvshow}
+                                }}
+                            ><i className="material-icons left">build</i>
+                                Edit TV Show
+                            </Link>
+                        </>
+                    }
+                </div>
+            </div>
+        </>
   ) 
 }
 
--- src/pages/TVShowList/TVShowList.jsx
+++ src/pages/TVShowList/TVShowList.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import './TVShowList.css'
 import * as tvshowAPI from '../../services/tvshows-api'
+import TVShowCard from '../../components/TVShowCard/TVShowCard'
 
 const TVShowList = (props) => {
 
   const [tvshows, setTvshows]= useState([])
+
+  async function handleDeleteTVShow(id){
+    await tvshowAPI.deleteOne(id)
+    setTvshows(tvshows.filter(t => t._id !== id))
+  }
 
   useEffect(() => {
     (async function(){
@@ -14,7 +20,18 @@ const TVShowList = (props) => {
   },[])
   
   return ( 
-    <h3>TV Show List</h3>
+    <>
+      <div className='TVShowList-grid'>
+        {tvshows.map(tvshow =>
+          <TVShowCard 
+            key={tvshow._id}
+            tvshow={tvshow}
+            user={props.user}
+            handleDeleteTVShow={handleDeleteTVShow}
+          />
+        )}
+      </div>
+    </>
   );
 }
  
